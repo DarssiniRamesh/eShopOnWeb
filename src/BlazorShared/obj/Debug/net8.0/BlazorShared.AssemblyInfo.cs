2b4a6ff59309c5ce14ec0e5d6db8668c89a9d94d
Kavia AI <production@kavia.ai>
2025-10-09 17:44:55 +0000

    Auto-commit: session cm3bb17b56 - eShopOnWeb

--- src/BlazorShared/obj/Debug/net8.0/BlazorShared.AssemblyInfo.cs
+++ src/BlazorShared/obj/Debug/net8.0/BlazorShared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorShared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c3dac594025c3d862ca38ef85c1a5649690d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a639c094ef12a46a07429a3d26c75622b50a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorShared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorShared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
